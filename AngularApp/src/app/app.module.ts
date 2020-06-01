4b12d2023f85ef938231529bea16b96104fd13b1
PedroG9 <pedrogarciatalavera1997@hotmail.com>
2020-06-01 17:54:09 +0200

    Creación AlojamientoComponent

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TipsComponent } from './tips/tips.component';
 import { ChatComponent } from './chat/chat.component';
+import { AlojamientoComponent } from './alojamiento/alojamiento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TipsComponent,
-    ChatComponent
+    ChatComponent,
+    AlojamientoComponent
   ],
   imports: [
     BrowserModule,
