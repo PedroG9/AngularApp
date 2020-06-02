9005ffa61278a2004dde65157a6b0f808ec1604b
Alejandro <ap96.gonzalez@gmail.com>
2020-06-02 19:02:32 +0200

    Html Home

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TipsComponent } from './tips/tips.component';
+import { AlojamientoComponent } from './alojamiento/alojamiento.component';
+import { ChatComponent } from './chat/chat.component';
+import { FormLoginComponent } from './form-login/form-login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'tips', component: TipsComponent },
+  { path: 'alojamiento', component: AlojamientoComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'login', component: FormLoginComponent },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
