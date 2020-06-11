0e905c3f9f9d0f53d80ebbb2cd0935b07893cc67
PedroG9 <pedrogarciatalavera1997@hotmail.com>
2020-06-11 17:59:18 +0200

    Cambios en Home y Alojamiento, creación de alojamiento.service y alojamiento.model

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +35,8 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularApp/src/app/models/alojamiento.model.ts
@@ -0,0 +1,9 @@
+export class Alojamiento {
+
+    ciudad: string;
+    tipo_alojamiento: string;
+    tipo_habitacion: string;
+    entrada: Date;
+    salida: Date;
+
+}
